25f4f14cb465400970105ec3f67200b8c841e840
qiushi <qiushi.yann@gmail.com>
2022-06-28 10:08:40 -0500

    no progress

--- R/wrap_h2o.R
+++ R/wrap_h2o.R
@@ -16,9 +16,9 @@ wrap.H2ORegressionModel <- function(x, ...) {
 wrap_h2o <- function(x, ...) {
     file_loc <- tempfile()
     if (x@have_mojo) {
-        file_loc <- h2o::h2o.save_mojo(x, path = file_loc)
+        file_loc <- with_no_progress(h2o::h2o.save_mojo(x, path = file_loc))
     } else {
-        file_loc <- h2o::h2o.saveModel(x, path = file_loc)
+        file_loc <- with_no_progress(h2o::h2o.saveModel(x, path = file_loc))
     }
     raw <- serialize(file_loc, connection = NULL)
     attr(raw, "mojo") <- x@have_mojo
@@ -43,9 +43,13 @@ unwrap.wrapped_H2ORegressionModel <- function(x, ...) {
 unwrap_h2o <- function(x, ...) {
   x <- unwrap_constr(x)
   if (attr(x, "mojo")) {
-    res <- h2o::h2o.import_mojo(unserialize(x))
+    res <- with_no_progress(h2o::h2o.import_mojo(unserialize(x)))
   } else {
-    res <- h2o::h2o.import_mojo(unserialize(x))
+    res <- with_no_progress(h2o::h2o.loadModel(unserialize(x)))
   }
   res
+}
+
+with_no_progress <- function(expr) {
+  rlang::eval_tidy(h2o:::with_no_h2o_progress(expr))
 }
