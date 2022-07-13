db4c301ed5f9982caf8f695743ddecaf6f1f6ac9
qiushi <qiushi.yann@gmail.com>
2022-07-13 14:34:06 -0500

    bundle recipe

--- R/bundle_recipe.R
+++ R/bundle_recipe.R
@@ -1,6 +1,17 @@
 #' @export
 bundle.recipe <- function(x, ...) {
-  res <- map(x$steps, bundle)
+  res <- x
+  steps_bundled <- purrr::map(res$steps, bundle::bundle)
+  res$steps <- steps_bundled
 
-  bundle_constr(res)
+  bundle_constr(
+    object = res,
+    situate = situate_constr(function(object) {
+      steps_unbundled <- purrr::map(object$steps, bundle::unbundle)
+      object$steps <- steps_unbundled
+      structure(object, class = !!class(x))
+    }),
+    desc_class = "recipe",
+    pkg_versions = c("recipes" = utils::packageVersion("recipes"))
+  )
 }
