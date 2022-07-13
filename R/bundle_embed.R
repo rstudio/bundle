73d2f92c4c2f926f7fd1dda55e05decd98f4c987
qiushi <qiushi.yann@gmail.com>
2022-07-13 14:34:25 -0500

    bundle `embed::step_umap`

---
+++ R/bundle_embed.R
@@ -0,0 +1,23 @@
+#' @export
+bundle.step_umap <- function(x, ...) {
+  rlang::check_installed("uwot")
+  rlang::check_dots_empty(...)
+
+  res <- x
+  file_loc <- tempfile()
+  uwot::save_uwot(res$object, file_loc)
+  raw <- serialize(file_loc, connection = NULL)
+  res$object <- raw
+
+  bundle_constr(
+    object = res,
+    situate = situate_constr(function(object) {
+      unserialized <- uwot::load_uwot(unserialize(object$object))
+      unserialized$mod_dir <- NULL
+      object$object <- unserialized
+      object
+    }),
+    desc_class = class(x)[1],
+    pkg_versions = c("uwot" = utils::packageVersion("uwot"))
+  )
+}
