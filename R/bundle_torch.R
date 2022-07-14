11d1e65a445d57d13060b87192af4f7a2bb85d8b
Simon P. Couch <simonpatrickcouch@gmail.com>
2022-07-14 15:50:26 -0400

    add torch support (#16)

---
+++ R/bundle_torch.R
@@ -0,0 +1,50 @@
+#' Torch bundlers
+#'
+#' For now, bundling methods for torch are only available
+#' via the luz package, "a higher level API for torch providing
+#' abstractions to allow for much less verbose training loops."
+#'
+#' These bundlers rely on serialization methods from luz and torch,
+#' which are described by the package authors as "experimental"
+#' and not for "use for long term storage."
+#'
+#' @method bundle luz_module_fitted
+#' @export
+bundle.luz_module_fitted <- function(x, ...) {
+  rlang::check_dots_empty()
+  rlang::check_installed("luz")
+  rlang::check_installed("torch")
+
+  res <- x
+
+  # see luz::luz_save and luz:::model_to_raw
+  suppressWarnings({
+    con <- rawConnection(raw(), open = "wr")
+    torch::torch_save(res$model, con)
+    serialized_model <- rawConnectionValue(con)
+    res$ctx$.serialized_model <- serialized_model
+    res$ctx$.serialization_version <- 2L
+  })
+
+  close(con)
+
+  bundle_constr(
+    object = res,
+    situate = situate_constr(function(object) {
+      # see luz::luz_load and luz:::model_from_raw
+      con <- rawConnection(object$ctx$.serialized_model)
+      on.exit({
+        close(con)
+      }, add = TRUE)
+      res <- torch::torch_load(con)
+
+      object$model <- res
+      object$ctx$.serialized_model <- NULL
+      object$ctx$.serialization_version <- NULL
+
+      structure(object, class = !!class(x))
+    }),
+    desc_class = class(x)[1],
+    pkg_versions = c("luz" = utils::packageVersion("luz"))
+  )
+}
