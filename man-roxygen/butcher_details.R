fe85d3b1cf0d642f844a47c41c4bc1f5ffb03045
Julia Silge <julia.silge@gmail.com>
2022-08-12 15:03:07 -0600

    Add link, tiny edits

--- man-roxygen/butcher_details.R
+++ man-roxygen/butcher_details.R
@@ -1,9 +1,9 @@
 #' @section bundle and butcher:
-#' butcher is an R package that allows users to remove parts of a fitted model
-#' object that are not needed for prediction.
+#' The [butcher](https://butcher.tidymodels.org/) package allows you to remove
+#' parts of a fitted model object that are not needed for prediction.
 #'
 #' This bundle method is compatible with pre-butchering. That is, for a
-#' fitted model `x`, users can safely call:
+#' fitted model `x`, you can safely call:
 #'
 #' ```
 #' res <-
