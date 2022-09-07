2c79797e61cdcb5a34a981917bd847e127a90bef
Julia Silge <julia.silge@gmail.com>
2022-09-07 15:38:31 -0600

    Tiny changes to docs

--- man-roxygen/title_desc.R
+++ man-roxygen/title_desc.R
@@ -1,6 +1,6 @@
 #' @title Bundle <%= class %> object
 #'
-#' @description Bundling a model prepares it to be saved to file and later
+#' @description Bundling a model prepares it to be saved to a file and later
 #'   restored for prediction in a new R session. See the 'Value' section for
 #'   more information on bundles and their usage.
 #'
